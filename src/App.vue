04982606eaf231ac83aaf968cf271a337163bd45
time-shimizu <213.f.r48132@gmail.com>
2021-02-27 18:10:57 +0900

    add scoped css

--- src/App.vue
+++ src/App.vue
@@ -14,3 +14,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  div {
+    border: 1px solid blue;
+  }
+</style>
--- src/components/LikeNumber.vue
+++ src/components/LikeNumber.vue
@@ -20,6 +20,9 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+  div {
+    border: 1px solid red;
+  }
 </style>
 
