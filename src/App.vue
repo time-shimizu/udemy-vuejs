c79abe492029fec5328f7a892ab211c8f3cd7854
time-shimizu <213.f.r48132@gmail.com>
2021-03-02 22:45:18 +0900

    props追加

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <LikeHeader></LikeHeader>
-    <LikeNumber></LikeNumber>
+    <h2>{{ number }}</h2>
+    <LikeNumber :number = "number"></LikeNumber>
+    <LikeNumber :number = "number"></LikeNumber>
   </div>
 </template>
 
@@ -9,6 +11,11 @@
 import LikeHeader from "./components/LikeHeader.vue";
 
 export default {
+  data() {
+    return {
+      number: 10
+    }
+  },
   components: {
     LikeHeader
   }
