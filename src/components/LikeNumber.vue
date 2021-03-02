0148d18e63fee8ad06f6d00aa5a58b027ad7f28e
time-shimizu <213.f.r48132@gmail.com>
2021-03-02 22:55:57 +0900

    propsのvalidation追加

--- src/components/LikeNumber.vue
+++ src/components/LikeNumber.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    <p>いいね（{{ number / 2 }}）</p>
+    <p>いいね（{{ halfNumber }}）</p>
     <button @click='increment'>+1</button>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['number'],
+  props: {
+    totalNumber: {
+      type: Number
+    },
+  },
+  computed: {
+    halfNumber () {
+      return this.totalNumber / 2
+    }
+  },
   methods: {
     increment() {
       this.number +=1;
